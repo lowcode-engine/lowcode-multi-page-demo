3f3e861883de62b18c8d4c4cacc44026c22bdf59
blueju <blueju@foxmail.com>
2022-08-11 13:43:56 +0000

    feat: 新增保存页面接口

--- server/index.js
+++ server/index.js
@@ -15,6 +15,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.resolve('pages/index.html'));
 });
 
+// 获取页面
 app.get('/pages', (req, res) => {
   res.json({
     code: 0,
@@ -22,15 +23,22 @@ app.get('/pages', (req, res) => {
   })
 });
 
-app.post('/page', (req, res) => {
+// 新建页面
+app.post('/page/:fileName', (req, res) => {
+  const { fileName = '' } = req.params
+  if (fileName.length === 0) {
+    res.json({
+      code: -1,
+      msg: '缺少参数：页面文件名'
+    })
+    return
+  }
   const { pageSchema = {} } = req.body
   if (Object.keys(pageSchema).length) {
-    console.log(pages.length);
     pages.push(pageSchema)
-    console.log(pages.length);
     res.json({
       code: 0,
-      msg: '创建新页面成功'
+      msg: '新页面创建成功'
     })
   } else {
     res.json({
@@ -40,7 +48,40 @@ app.post('/page', (req, res) => {
   }
 });
 
+// 保存页面
+app.put('/page/:fileName', (req, res) => {
+  const { fileName = '' } = req.params
+  if (fileName.length === 0) {
+    res.json({
+      code: -1,
+      msg: '页面保存失败，缺少参数：页面文件名'
+    })
+    return
+  }
+  const { pageSchema = {} } = req.body
+  if (Object.keys(pageSchema).length) {
+    const index = pages.findIndex(item => item.fileName === pageSchema.fileName)
+    if (index === -1) {
+      res.json({
+        code: -1,
+        msg: '页面保存失败，未找到要保存/更新的页面'
+      })
+      return
+    }
+    pages.splice(index, 1, pageSchema)
+    res.json({
+      code: 0,
+      msg: `页面保存成功`
+    })
+  } else {
+    res.json({
+      code: -1,
+      msg: '缺少参数：页面描述 Schema'
+    })
+  }
+});
 
+// 删除页面
 app.delete('/page/:fileName', (req, res) => {
   const { fileName = '' } = req.params
   if (fileName.length) {
